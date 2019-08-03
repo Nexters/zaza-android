a7203d5ece0cb710351b8803476bab5b4c491cfc
Jongkook <jongkook@Jongkooks-MacBook-Pro.local>
2019-08-03 18:12:06 +0900

    LiveData 적용

--- app/src/main/java/com/teamnexters/zaza/base/BaseActivity.kt
+++ app/src/main/java/com/teamnexters/zaza/base/BaseActivity.kt
@@ -23,14 +23,14 @@ abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
 /*
 
     */
-/**
+    /**
      * viewModel 로 쓰일 변수.
      *//*
 
     abstract val viewModel: R
 
     */
-/**
+    /**
      * 레이아웃을 띄운 직후 호출.
      * 뷰나 액티비티의 속성 등을 초기화.
      * ex) 리사이클러뷰, 툴바, 드로어뷰..
@@ -39,7 +39,7 @@ abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
     abstract fun initStartView()
 
     */
-/**
+    /**
      * 두번째로 호출.
      * 데이터 바인딩 및 rxjava 설정.
      * ex) rxjava observe, databinding observe..
@@ -48,7 +48,7 @@ abstract class BaseActivity<T : ViewDataBinding> : AppCompatActivity() {
     abstract fun initDataBinding()
 
     */
-/**
+    /**
      * 바인딩 이후에 할 일을 여기에 구현.
      * 그 외에 설정할 것이 있으면 이곳에서 설정.
      * 클릭 리스너도 이곳에서 설정.
--- app/src/main/java/com/teamnexters/zaza/sample/SampleViewModel.kt
+++ app/src/main/java/com/teamnexters/zaza/sample/SampleViewModel.kt
@@ -1,8 +1,25 @@
 package com.teamnexters.zaza.sample
 
+import androidx.lifecycle.MutableLiveData
 import com.teamnexters.zaza.base.BaseViewModel
 
-class SampleViewModel(sample: Sample) : BaseViewModel() {
-    val nameTag = sample.name + "님"
-    val jobTag = "(" + sample.job + ")"
+class SampleViewModel : BaseViewModel() {
+    val name: String = String()
+    val job: String = String()
+
+    val nameTag: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+    lateinit var jobTag: MutableLiveData<String>
+
+    init {
+        nameTag.value = "WATING"
+    }
+
+    fun getName(): MutableLiveData<String> {
+        if (nameTag == null) {
+//            nameTag = MutableLiveData()
+        }
+        return nameTag
+    }
 }
