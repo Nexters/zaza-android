746b2299710a116e15f91c2961d78a0aedadab67
Park JiHye <youo78@naver.com>
2019-08-03 18:30:48 +0900

    alarm bottomSheetDialog add

--- app/src/main/java/com/teamnexters/zaza/ui/alarm/AlarmActivity.kt
+++ app/src/main/java/com/teamnexters/zaza/ui/alarm/AlarmActivity.kt
@@ -2,12 +2,30 @@ package com.teamnexters.zaza.ui.alarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.teamnexters.zaza.R
+import com.teamnexters.zaza.databinding.ActivityAlarmBinding
 
 class AlarmActivity : AppCompatActivity() {
+
+    private var layoutRes = getRayoutRes()
+    private lateinit var binding : ActivityAlarmBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm)
+
+        val bottomSheetDialog = AlarmBottomSheetDialog().getInstance()
+
+        bottomSheetDialog.show(supportFragmentManager,"bottomSheet");
+
+    }
+
+    fun subscribeUi(){
+
+    }
+
+    fun getRayoutRes() : Int{
+        return R.layout.activity_alarm
     }
 }
---
+++ app/src/main/java/com/teamnexters/zaza/ui/alarm/AlarmViewModel.kt
@@ -0,0 +1,14 @@
+package com.teamnexters.zaza.ui.alarm
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import com.teamnexters.zaza.sample.DisposableViewModel
+import com.teamnexters.zaza.sample.Sample
+import com.teamnexters.zaza.sample.SampleViewModel
+
+class AlarmViewModel: DisposableViewModel() {
+
+
+
+}
