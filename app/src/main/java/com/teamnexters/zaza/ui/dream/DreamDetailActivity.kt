91439a3d17eb53889ff6ce10e53f8a257a3a5252
Rowoosuk <lkjs8269@naver.com>
2019-08-09 20:24:37 +0900

    Add dream detail page and squareRelativeLayout(Customized)
    1. I added a page that represents dream detail view.
    2. To link dream activity, I temporary added a button "꿈 페이지"
    3. I added a class "SquareRelativeLayout" to adjust evenly in Recyclerview(GridLayout).

---
+++ app/src/main/java/com/teamnexters/zaza/ui/dream/DreamDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.teamnexters.zaza.ui.dream
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DreamDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dream_detail)
+    }
+}
