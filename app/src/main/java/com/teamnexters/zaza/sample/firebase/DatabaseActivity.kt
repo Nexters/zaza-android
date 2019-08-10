d5844e3b3c14d3890c43f628a8736c1f7c681bc8
Juhyun Lim <wow5468@naver.com>
2019-08-10 17:43:21 +0900

    Merge branch 'jh' of github.com:Nexters/zaza-android
    # Conflicts:
    #	.idea/misc.xml

---
+++ app/src/main/java/com/teamnexters/zaza/sample/firebase/DatabaseActivity.kt
@@ -0,0 +1,13 @@
+package com.teamnexters.zaza.sample.firebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teamnexters.zaza.R
+
+class DatabaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_database)
+    }
+}
